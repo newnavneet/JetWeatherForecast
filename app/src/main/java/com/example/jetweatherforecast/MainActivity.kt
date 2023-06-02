fccf4a6679b40c8628f551c2ee6c21ce57c32326
Nav511 <125399571+Nav511@users.noreply.github.com>
2023-06-03 01:33:21 +0530

    initial commit

--- app/src/main/java/com/example/jetweatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/jetweatherforecast/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.jetweatherforecast
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetweatherforecast.navigation.WeatherNavigation
 import com.example.jetweatherforecast.ui.theme.JetWeatherForecastTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,18 +23,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetWeatherForecastTheme {
-
-                Surface(color = MaterialTheme.colorScheme.background) {
-
-                }
-            }
+            WeatherApp()
+           }
         }
     }
+
+
+@Composable
+fun WeatherApp() {
+    JetWeatherForecastTheme {
+
+        Surface(color = MaterialTheme.colorScheme.background,
+             modifier = Modifier.fillMaxSize()) {
+            Column(verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                WeatherNavigation()
+
+            }
+
+        }
+
+
+    }
 }
-
-
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
