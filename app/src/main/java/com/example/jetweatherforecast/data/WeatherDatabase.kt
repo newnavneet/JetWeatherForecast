8de0495e14fc098b83e81f5f30f1be7b55b952bc
newnavneet <navneetnv.sharma.00@gmail.com>
2023-07-07 19:26:08 +0530

    Create About favorites and settings screen

---
+++ app/src/main/java/com/example/jetweatherforecast/data/WeatherDatabase.kt
@@ -0,0 +1,10 @@
+package com.example.jetweatherforecast.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.jetweatherforecast.model.Favorite
+
+@Database(entities = [Favorite::class, Unit::class], version = 2, exportSchema = false)
+abstract class WeatherDatabase: RoomDatabase() {
+    abstract fun weatherDao(): WeatherDao
+}
---
+++ app/src/main/java/com/example/jetweatherforecast/screens/favorites/FavoritesScreen.kt
@@ -0,0 +1,25 @@
+package com.example.jetweatherforecast.screens.favorites
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.NavController
+import com.example.jetweatherforecast.R
+
+@Composable
+fun FavoritesScreen(navController: NavController){
+    Text(text = "Favorites...")
+}
+ 
