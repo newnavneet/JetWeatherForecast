2d61af362a48a82e13aa2b1e204f1f4713b45e04
newnavneet <navneetnv.sharma.00@gmail.com>
2023-07-10 15:34:40 +0530

    Creating the Favoritescreen

--- app/src/main/java/com/example/jetweatherforecast/screens/favorites/FavoriteViewModel.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/favorites/FavoriteViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.viewModelScope
 import com.example.jetweatherforecast.model.Favorite
 import com.example.jetweatherforecast.repository.WeatherDbRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
@@ -14,7 +13,7 @@ import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-@OptIn(InternalCoroutinesApi::class)
+
 class FavoriteViewModel @Inject constructor(private val repository: WeatherDbRepository)
     : ViewModel(){
     private val _favList = MutableStateFlow<List<Favorite>>(emptyList())
@@ -28,20 +27,17 @@ class FavoriteViewModel @Inject constructor(private val repository: WeatherDbRep
                         Log.d("TAG", ": Empty favs ")
                     }else {
                         _favList.value = listofFavs
-                        Log.d("Favs", ":${favList.value} ")
+                        Log.d("FAVS", ":${favList.value} ")
                     }
                 }
 
         }
     }
-    fun insertFavorite(favorite: Favorite) = viewModelScope.launch {
-        repository.insertFavorite(favorite) }
+    fun insertFavorite(favorite: Favorite) = viewModelScope.launch { repository.insertFavorite(favorite) }
 
-    fun updateFavorite(favorite: Favorite) = viewModelScope.launch {
-        repository.updateFavorite(favorite) }
+    fun updateFavorite(favorite: Favorite) = viewModelScope.launch { repository.updateFavorite(favorite) }
 
-    fun deleteFavorite(favorite: Favorite) = viewModelScope.launch {
-        repository.deleteFavorite(favorite) }
+    fun deleteFavorite(favorite: Favorite) = viewModelScope.launch { repository.deleteFavorite(favorite) }
 
 
 
