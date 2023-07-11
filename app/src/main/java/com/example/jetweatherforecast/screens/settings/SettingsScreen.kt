3e8747c99e3f253ce32f0f34e6f189a532a6ab15
newnavneet <navneetnv.sharma.00@gmail.com>
2023-07-11 14:35:30 +0530

    Creating the settingscreen

--- app/src/main/java/com/example/jetweatherforecast/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/settings/SettingsScreen.kt
@@ -1,10 +1,118 @@
 package com.example.jetweatherforecast.screens.settings
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.IconToggleButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.jetweatherforecast.model.Unit
+import com.example.jetweatherforecast.widgets.WeatherAppBar
 
 @Composable
-fun SettingsScreen(navController: NavController){
-    Text(text = "Settig...")
+fun SettingsScreen(navController: NavController,
+                    settingsViewModel: SettingsViewModel = hiltViewModel()){
+
+    var unitToggleState by remember { mutableStateOf(false) }
+    val measurementUnits = listOf("Imperial (F)", "Metric (C)")
+    val choiceFromDb = settingsViewModel.unitList.collectAsState().value
+
+    val defaultChoice = if (choiceFromDb.isNullOrEmpty()) measurementUnits[0]
+    else choiceFromDb[0].unit
+
+    var choiceState by remember {
+        mutableStateOf(defaultChoice)
+    }
+    
+    Scaffold(topBar = {
+        WeatherAppBar(
+        title = "Settings",
+        icon = Icons.Default.ArrowBack,
+        false,
+        navController = navController){
+        navController.popBackStack()
+    }
+    }) {
+        Surface(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()) {
+            Column(verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = "Change Units of Measurement",
+                    modifier = Modifier.padding(bottom = 15.dp)
+                )
+
+                IconToggleButton(checked = !unitToggleState ,
+                    onCheckedChange = {
+                        unitToggleState = !it
+                        choiceState = if (unitToggleState) {
+                            "Imperial (F)"
+                        } else {
+                            "Metric (C)"
+                        }
+                        Log.d("TAG", "MainContent: $unitToggleState")
+
+                    }, modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .clip(shape = RectangleShape)
+                        .padding(5.dp)
+                        .background(Color.Magenta.copy(alpha = 0.4f))) {
+
+                    Text(text = if (unitToggleState) "Fahrenheit ºF" else "Celsius ºC" )
+
+
+                }
+                Button(onClick = {
+                    settingsViewModel.deleteAllUnits()
+                    settingsViewModel.insertUnit(Unit(unit = choiceState ))
+
+                },
+                    modifier = Modifier
+                        .padding(3.dp)
+                        .align(Alignment.CenterHorizontally),
+                    shape = RoundedCornerShape(34.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color(0xFFEFBE42)
+                    )) {
+                    Text(text = "Save",
+                        modifier = Modifier.padding(4.dp),
+                        color = Color.White,
+                        fontSize = 17.sp)
+
+                }
+
+
+            }
+        }
+
+
+    }
+
 }
+
